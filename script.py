c704821389379b07769a5556bcc5220fd3805ad8
Senay Yakut <senaykt@gmail.com>
2020-09-04 22:29:17 -0700

    Solution for Basta Fazoolin Problem Set

---
+++ script.py
@@ -0,0 +1,71 @@
+#create a menu
+class Menu:
+  def __init__(self, name, items, start_time, end_time):
+    self.name = name
+    self.items = items
+    self.start_time = start_time
+    self.end_time = end_time
+
+  def __repr__(self):
+    return self.name + " menu available from " + str(self.start_time) + " to " + str(self.end_time) 
+
+  def calculate_bill(self, purchased_items):
+    bill = 0
+    for purchased_item in purchased_items:
+       if purchased_item in self.items:
+          bill += self.items[purchased_item]
+    return bill   
+
+
+#Brunch menu
+brunch = Menu("brunch", {
+  'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50
+}, 1100, 1600)  
+
+#earl_bird
+early_bird = Menu("early-bird dinner", {
+  'salumeria plate': 8.00, 'salad and breadsticks (serves 2, no refills)': 14.00, 'pizza with quattro formaggi': 9.00, 'duck ragu': 17.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 1.50, 'espresso': 3.00,
+}, 1500, 1800)
+
+#dinner
+dinner = Menu("dinner", {
+  'crostini with eggplant caponata': 13.00, 'ceaser salad': 16.00, 'pizza with quattro formaggi': 11.00, 'duck ragu': 19.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 2.00, 'espresso': 3.00,
+}, 1700, 2300)
+
+#kids
+kids = Menu("kids menu", {
+  'chicken nuggets': 6.50, 'fusilli with wild mushrooms': 12.00, 'apple juice': 3.00
+}, 1100, 2100)
+
+print(brunch.calculate_bill(["pancakes", "home fries", "coffee"]))
+
+print(early_bird.calculate_bill(['salumeria plate','mushroom ravioli (vegan)']))
+
+
+class Franchise:
+  def __init__(self,address, menus):
+    self.address = address
+    self.menus = menus
+
+  def __repr__(self):
+    return self.address
+
+  def available_menus(self, time):
+    available_menues = []
+    for menu in self.menus:
+      if time >= menu.start_time and time <= menu.end_time:
+        available_menues.append(menu)
+    return available_menues    
+
+flagship_store = Franchise("1232 West End Road", ["brunch","early_bird","dinner","kids"])
+new_installment = Franchise("12 East Mulberry Street", ["brunch","early_bird","dinner","kids"])
+
+print(flagship_store)
+print(new_installment.available_menus(1100))
+
+
+
+
+
+
+
