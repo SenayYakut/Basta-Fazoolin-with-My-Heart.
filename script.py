b05341cf695e56bde37615c83916f5e2d75edc19
Senay Yakut <senaykt@gmail.com>
2020-09-06 22:42:46 -0700

    Solution for Basta Fazoolin Solution

--- script.py
+++ script.py
@@ -18,24 +18,34 @@ class Menu:
 
 
 #Brunch menu
-brunch = Menu("brunch", {
+brunch_items = {
   'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50
-}, 1100, 1600)  
+}
+
+brunch = Menu("Brunch", brunch_items, 1100, 1600)  
 
 #earl_bird
-early_bird = Menu("early-bird dinner", {
+early_bird_items = {
   'salumeria plate': 8.00, 'salad and breadsticks (serves 2, no refills)': 14.00, 'pizza with quattro formaggi': 9.00, 'duck ragu': 17.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 1.50, 'espresso': 3.00,
-}, 1500, 1800)
+}
+
+early_bird = Menu("Early-bird Dinner",early_bird_items, 1500, 1800)
 
 #dinner
-dinner = Menu("dinner", {
+dinner_items = {
   'crostini with eggplant caponata': 13.00, 'ceaser salad': 16.00, 'pizza with quattro formaggi': 11.00, 'duck ragu': 19.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 2.00, 'espresso': 3.00,
-}, 1700, 2300)
+}
+
+dinner = Menu("Dinner", dinner_items, 1700, 2300)
 
 #kids
-kids = Menu("kids menu", {
+kids_items = {
   'chicken nuggets': 6.50, 'fusilli with wild mushrooms': 12.00, 'apple juice': 3.00
-}, 1100, 2100)
+}
+
+kids = Menu("Kids menu", kids_items, 1100, 2100)
+
+print(brunch)
 
 print(brunch.calculate_bill(["pancakes", "home fries", "coffee"]))
 
@@ -51,21 +61,38 @@ class Franchise:
     return self.address
 
   def available_menus(self, time):
-    available_menues = []
+    available_menu = []
     for menu in self.menus:
       if time >= menu.start_time and time <= menu.end_time:
-        available_menues.append(menu)
-    return available_menues    
+        available_menu.append(menu)
+    return available_menu   
 
-flagship_store = Franchise("1232 West End Road", ["brunch","early_bird","dinner","kids"])
-new_installment = Franchise("12 East Mulberry Street", ["brunch","early_bird","dinner","kids"])
+menus = [brunch,early_bird,dinner,kids]
+flagship_store = Franchise("1232 West End Road", menus)
+new_installment = Franchise("12 East Mulberry Street", menus)
 
 print(flagship_store)
-print(new_installment.available_menus(1100))
+print(new_installment)
+print(flagship_store.available_menus(1100))
+print(flagship_store.available_menus(1700))
 
 
+class Business:
+  def __init__(self, name, franchises):
+    self.name = name
+    self.franchises = franchises
+
+basta = Business("Basta Fazoolin' with my Heart", ["flagship_store","new_installment"])
 
 
+arepa_items = {
+  'arepa pabellon': 7.00, 'pernil arepa': 8.50, 'guayanes arepa': 8.00, 'jamon arepa': 7.50
+}
 
+arepa_menu = Menu("Take a’ Arepa", arepa_items, 1000, 2000)
 
+arepas_place= Franchise("189 Fitzgerald Avenue", arepa_menu)
 
+arepa = Business("Take a' Arepa",arepas_place)
+
+print(arepas_place)
